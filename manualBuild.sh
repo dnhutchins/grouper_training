c402bf80b96c0ae5ef253f9fd95845a1b6424db1
John Gasper <jtgasper3@gmail.com>
2018-07-10 16:40:52 -0700

    rebasing image names

--- manualBuild.sh
+++ manualBuild.sh
@@ -1,7 +1,7 @@
-docker build --tag=tier/grouper_training_base:latest base/ \
-&& docker build --tag=tier/grouper_training_ex1:latest exercise1 \
-&& docker build --tag=tier/grouper_training_ex2:latest exercise2 \
-&& docker build --tag=tier/grouper_training_full_demo:latest full-demo
+docker build --tag=tier/grouper_training_env:base base/ \
+&& docker build --tag=tier/grouper_training_env:ex1 exercise1 \
+&& docker build --tag=tier/grouper_training_env:ex2 exercise2 \
+&& docker build --tag=tier/grouper_training_env:full_demo full-demo
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   say build complete
