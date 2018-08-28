799dbbdaaddc23cd31ef7bcadaec2f54970fa03a
John Gasper <jtgasper3@gmail.com>
2018-08-27 17:02:35 -0700

    Completed: ex401.2.1, 401.2.2, 401.2.3, and 401.2.4

--- manualBuild.sh
+++ manualBuild.sh
@@ -7,6 +7,10 @@ docker build --pull --tag=tier/grouper-training-env:base base/ \
 && docker build --tag=tier/grouper-training-env:ex401.1.5 ex401/ex401.1.5 \
 && docker build --tag=tier/grouper-training-env:ex401.1.6 ex401/ex401.1.6 \
 && docker build --tag=tier/grouper-training-env:ex401.1.end ex401/ex401.1.end \
+&& docker build --tag=tier/grouper-training-env:ex401.2.1 ex401/ex401.2.1 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.2 ex401/ex401.2.2 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.3 ex401/ex401.2.3 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.4 ex401/ex401.2.4
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   say build complete
