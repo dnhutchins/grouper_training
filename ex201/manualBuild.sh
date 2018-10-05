f72340b421e0c0cd6b9cc6ae8a1a960bef508836
John Gasper <jtgasper3@gmail.com>
2018-10-05 12:30:56 -0700

    Refactoring the manualBuild process.

---
+++ ex201/manualBuild.sh
@@ -0,0 +1,14 @@
+docker build --tag=tier/grouper-training-env:ex201.1.1 ex201.1.1 \
+&& docker build --tag=tier/grouper-training-env:ex201.1.end ex201.1.end \
+&& docker build --tag=tier/grouper-training-env:ex201.2.1 ex201.2.1 \
+&& docker build --tag=tier/grouper-training-env:ex201.2.end ex201.2.end \
+&& docker build --tag=tier/grouper-training-env:ex201.3.1 ex201.3.1 \
+&& docker build --tag=tier/grouper-training-env:ex201.3.end ex201.3.end \
+&& docker build --tag=tier/grouper-training-env:ex201.4.1 ex201.4.1 \
+&& docker build --tag=tier/grouper-training-env:ex201.4.end ex201.4.end \
+&& docker build --tag=tier/grouper-training-env:ex201.5.1 ex201.5.1 \
+&& docker build --tag=tier/grouper-training-env:ex201.5.end ex201.5.end
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  say exercises for 201 build complete
+fi
---
+++ ex401/manualBuild.sh
@@ -0,0 +1,31 @@
+docker build --tag=tier/grouper-training-env:ex401.1.1 ex401.1.1 \
+&& docker build --tag=tier/grouper-training-env:ex401.1.2 ex401.1.2 \
+&& docker build --tag=tier/grouper-training-env:ex401.1.3 ex401.1.3 \
+&& docker build --tag=tier/grouper-training-env:ex401.1.4 ex401.1.4 \
+&& docker build --tag=tier/grouper-training-env:ex401.1.5 ex401.1.5 \
+&& docker build --tag=tier/grouper-training-env:ex401.1.6 ex401.1.6 \
+&& docker build --tag=tier/grouper-training-env:ex401.1.end ex401.1.end \
+&& docker build --tag=tier/grouper-training-env:ex401.2.1 ex401.2.1 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.2 ex401.2.2 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.3 ex401.2.3 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.4 ex401.2.4 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.5 ex401.2.5 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.6 ex401.2.6 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.7 ex401.2.7 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.8 ex401.2.8 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.9 ex401.2.9 \
+&& docker build --tag=tier/grouper-training-env:ex401.2.end ex401.2.end \
+&& docker build --tag=tier/grouper-training-env:ex401.3.1 ex401.3.1 \
+&& docker build --tag=tier/grouper-training-env:ex401.3.2 ex401.3.2 \
+&& docker build --tag=tier/grouper-training-env:ex401.3.3 ex401.3.3 \
+&& docker build --tag=tier/grouper-training-env:ex401.3.4 ex401.3.4 \
+&& docker build --tag=tier/grouper-training-env:ex401.3.5 ex401.3.5 \
+&& docker build --tag=tier/grouper-training-env:ex401.3.6 ex401.3.6 \
+&& docker build --tag=tier/grouper-training-env:ex401.3.7 ex401.3.7 \
+&& docker build --tag=tier/grouper-training-env:ex401.3.end ex401.3.end \
+&& docker build --tag=tier/grouper-training-env:ex401.4.1 ex401.4.1 \
+&& docker build --tag=tier/grouper-training-env:ex401.4.end ex401.4.end
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  say exercises for 401 build complete
+fi
