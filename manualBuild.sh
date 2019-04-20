05e30eaa867c54e9b60289cbbfb9ac4253403daf
William G. Thompson, Jr <wgthom@gmail.com>
2019-04-20 13:56:55 -0400

    Update manualBuild.sh

--- manualBuild.sh
+++ manualBuild.sh
@@ -1,9 +1,7 @@
 source ./buildVersion.sh
 echo "Building tier/gte version ${VERSION_TAG}"
-docker build --pull --tag=tier/gte:base-${VERSION_TAG} base/
-
-# \
-#&& docker build --tag=tier/gte:full_demo-${VERSION_TAG} full-demo \
+docker build --pull --tag=tier/gte:base-${VERSION_TAG} base/ \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:full_demo-${VERSION_TAG} full-demo \
 
 pushd ex101
 ./manualBuild.sh
@@ -12,8 +10,6 @@ popd
 pushd ex201
 ./manualBuild.sh
 popd
-
-exit 1
 
 pushd ex211
 ./manualBuild.sh
