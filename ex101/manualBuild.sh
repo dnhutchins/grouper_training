41162def8ee681c9a7cb6429d948a86c50a757a8
William G. Thompson, Jr <wgthom@gmail.com>
2019-04-20 12:29:18 -0400

    updated 101 build to use VERSION_TAG

--- ex101/manualBuild.sh
+++ ex101/manualBuild.sh
@@ -1,4 +1,6 @@
-docker build --tag=tier/gte:101-$tag ex101-$tag
+source ../buildVersion.sh
+echo "Building gte:101 version ${VERSION_TAG}"
+docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:101.1.1-${VERSION_TAG} ex101.1.1
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   say exercises for 101 build complete
