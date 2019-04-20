26b8dde28dce8ab90fbaccb20c451964a369b22d
William G. Thompson, Jr <wgthom@gmail.com>
2019-04-20 12:29:34 -0400

    updated to use VERSION_TAG

--- ex201/manualBuild.sh
+++ ex201/manualBuild.sh
@@ -1,13 +1,15 @@
-docker build --tag=tier/gte:ex201.1.1-$tag ex201.1.1-$tag \
-&& docker build --tag=tier/grouper-training-env:ex201.1.end ex201.1.end \
-&& docker build --tag=tier/grouper-training-env:ex201.2.1 ex201.2.1 \
-&& docker build --tag=tier/grouper-training-env:ex201.2.end ex201.2.end \
-&& docker build --tag=tier/grouper-training-env:ex201.3.1 ex201.3.1 \
-&& docker build --tag=tier/grouper-training-env:ex201.3.end ex201.3.end \
-&& docker build --tag=tier/grouper-training-env:ex201.4.1 ex201.4.1 \
-&& docker build --tag=tier/grouper-training-env:ex201.4.end ex201.4.end \
-&& docker build --tag=tier/grouper-training-env:ex201.5.1 ex201.5.1 \
-&& docker build --tag=tier/grouper-training-env:ex201.5.end ex201.5.end
+source ../buildVersion.sh
+echo "Building gte:201 version ${VERSION_TAG}"
+docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.1.1-${VERSION_TAG} ex201.1.1 \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.1.end-${VERSION_TAG} ex201.1.end \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.2.1-${VERSION_TAG} ex201.2.1 \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.2.end-${VERSION_TAG} ex201.2.end \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.3.1-${VERSION_TAG} ex201.3.1 \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.3.end-${VERSION_TAG} ex201.3.end \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.4.1-${VERSION_TAG} ex201.4.1 \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.4.end-${VERSION_TAG} ex201.4.end \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.5.1-${VERSION_TAG} ex201.5.1 \
+&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:201.5.end-${VERSION_TAG} ex201.5.end
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   say exercises for 201 build complete
