6a2e1b008253ab528627ce7f20b49352bb9109ad
mchyzer <mchyzer@macbook-pro-103.lan>
2020-05-29 01:23:09 -0400

    update init scripts

--- internal/mkstudent.sh
+++ internal/mkstudent.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+echo "$GROUPER_GTE_BRANCH" > grouperGteBranch.txt
+chmod a+r grouperGteBranch.txt
+echo "$GROUPER_GTE_DOCKER_BRANCH" > grouperGteDockerBranch.txt
+chmod a+r grouperGteDockerBranch.txt
+
+yum -y update
+yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel wget mlocate emacs nano nslookup mlocate patch
+
+yum -y install docker
+
+systemctl start docker
+
+docker pull "tier/gte:401.4.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:401.4.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:401.3.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:401.3.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:401.2.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:401.2.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:401.1.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:401.1.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:301.4.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:211.1.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.5.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.5.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.4.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.4.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.3.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.3.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.2.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.2.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.1.end-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:201.1.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:101.1.1-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:full_demo-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull "tier/gte:base-$GROUPER_GTE_DOCKER_BRANCH"
+docker pull rabbitmq:management
+
+
 # Who am i?
 MY_IP=$(curl icanhazip.com)
 
@@ -16,10 +54,41 @@ useradd -u 1002 -g 1002 -m student
 echo "student:$PASS" | chpasswd
 
 # Add the student account to docker group.
-usermod -aG docker student
+#usermod -aG docker student
 
 # Student uses sudo to become root. 
-#usermod -aG wheel student
+usermod -G wheel,docker student
+
+echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/99wheel
+
+sed -i "s|PasswordAuthentication no|PasswordAuthentication yes|g" /etc/ssh/sshd_config
+
+systemctl restart sshd.service
+
+cd /home/student/
+
+echo >> /home/student/.bashrc
+echo 'export PATH=/home/student:$PATH' >> /home/student/.bashrc
+echo >> /home/student/.bashrc
+
+wget "https://github.internet2.edu/docker/grouper_training/raw/$GROUPER_GTE_BRANCH/gte"
+wget "https://github.internet2.edu/docker/grouper_training/raw/$GROUPER_GTE_BRANCH/gte-gsh"
+wget "https://github.internet2.edu/docker/grouper_training/raw/$GROUPER_GTE_BRANCH/gte-logs"
+wget "https://github.internet2.edu/docker/grouper_training/raw/$GROUPER_GTE_BRANCH/gte-shell"
+wget "https://github.internet2.edu/docker/grouper_training/raw/$GROUPER_GTE_BRANCH/start-rabbitmq.sh"
+wget "https://github.internet2.edu/docker/grouper_training/raw/$GROUPER_GTE_BRANCH/README.md"
+
+chown student.student /home/student/*
+
+chmod +x /home/student/gte
+chmod +x /home/student/gte-gsh
+chmod +x /home/student/gte-logs
+chmod +x /home/student/gte-shell
+chmod +x /home/student/start-rabbitmq.sh
+
+
+updatedb
 
 # Echo the password
 echo "$MY_IP, student, $PASS"
+
--- internal/userdata.sh
+++ internal/userdata.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-yum -y update
+
+export GROUPER_GTE_BRANCH=202006-post
+#export GROUPER_GTE_DOCKER_BRANCH=$GROUPER_GTE_BRANCH
+export GROUPER_GTE_DOCKER_BRANCH=202006
+
+yum -y install wget
+cd /root
+wget "https://github.internet2.edu/docker/grouper_training/raw/$GROUPER_GTE_BRANCH/internal/mkstudent.sh"
+chmod +x mkstudent.sh
 /root/mkstudent.sh
-usermod -G wheel,docker student
-docker pull rabbitmq:management
-/home/student/start-rabbitmq.sh 
