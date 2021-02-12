c1148a761e6ecc29d09f4cd97fb74a782d84dfc2
Chris Hyzer <mchyzer@isc.upenn.edu>
2021-02-12 10:42:12 -0500

    upgrade to 2.5.42

--- internal/passwordsToSpreadsheet.sh
+++ internal/passwordsToSpreadsheet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws ec2 describe-instances | jq '[.Reservations | .[] | .Instances | .[] | select(.State.Name!="terminated") | select((.Tags[]|select(.Key=="env")|.Value) =="grouperTraining")] | .[] | .InstanceId' | xargs -n 1 -I{}  aws  ec2 get-console-output  --instance-id {} | gawk 'match($0, /abcdefg12345678,([0-9.]+),([0-9a-zA-Z]+),/, m) { print m[1], ",student,", m[2], ",ssh -L 8443:localhost:8443 -l student ", m[1]; }'
+ aws ec2 describe-instances --filters "Name=tag-key,Values=env" --filters "Name=tag-value,Values=grouperTraining" |jq '[.Reservations | .[] | .Instances | .[] | select(.State.Name!="terminated") ] | .[] | .InstanceId' | xargs -n 1 -I{}  aws  ec2 get-console-output  --instance-id {} | gawk 'match($0, /abcdefg12345678,([0-9.]+),([0-9a-zA-Z]+),/, m) { print m[1], ",student,", m[2], ",ssh -L 8443:localhost:8443 -l student ", m[1]; }'
+ 
