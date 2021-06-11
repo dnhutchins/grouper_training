daad142abf6277a6509c0cf1113f596824f6379f
Chad Redman <chad_redman@unc.edu>
2021-06-11 00:32:53 -0400

    Don't build 401.5 and 401.7 since not teaching June 2021

--- ex401/manualBuild.sh
+++ ex401/manualBuild.sh
@@ -4,10 +4,10 @@ docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.1.1-${VER
 && docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.1.end-${VERSION_TAG} ex401.1.end \
 && docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.3.1-${VERSION_TAG} ex401.3.1 \
 && docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.3.end-${VERSION_TAG} ex401.3.end \
-&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.5.1-${VERSION_TAG} ex401.5.1 \
-&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.5.end-${VERSION_TAG} ex401.5.end \
-&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.7.1-${VERSION_TAG} ex401.7.1 \
-&& docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.7.end-${VERSION_TAG} ex401.7.end
+# && docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.5.1-${VERSION_TAG} ex401.5.1 \
+# && docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.5.end-${VERSION_TAG} ex401.5.end \
+# && docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.7.1-${VERSION_TAG} ex401.7.1 \
+# && docker build --build-arg VERSION_TAG=${VERSION_TAG} --tag=tier/gte:401.7.end-${VERSION_TAG} ex401.7.end
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   say exercises for 401 build complete
